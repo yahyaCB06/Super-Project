c1b324404f6bb7757aaf44c366afb8a4148d1169
yahyaCB06 <yahyamahassine007@gmail.com>
2025-02-28 22:42:39 +0000

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "sshKey.pem" 
+  key_name      = "sshKey" 
   tags = {
     Name = var.instance_name
   }
